4c4b976bb8586ce3137dca56e79a8656f5ad4f5f
rachit173 <rtibrewal@wisc.edu>
2022-02-26 06:43:08 -0600

    Add client request code.

--- client/client_main.cc
+++ client/client_main.cc
@@ -24,6 +24,41 @@ using grpc::Status;
 using afs::Greeter;
 using afs::HelloRequest;
 using afs::HelloReply;
+class GreeterClient {
+ public:
+  GreeterClient(std::shared_ptr<Channel> channel)
+      : stub_(Greeter::NewStub(channel)) {}
+
+  // Assembles the client's payload, sends it and presents the response back
+  // from the server.
+  std::string SayHello(const std::string& user) {
+    // Data we are sending to the server.
+    HelloRequest request;
+    request.set_name(user);
+
+    // Container for the data we expect from the server.
+    HelloReply reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->SayHello(&context, request, &reply);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return reply.message();
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return "RPC failed";
+    }
+  }
+
+ private:
+  std::unique_ptr<Greeter::Stub> stub_;
+};
 // include in one .cpp file
 // #include "Fuse-impl.h"
 // #include "Fuse.h"
@@ -144,5 +179,11 @@ int main(int argc, char* argv[]) {
  	struct fuse_args args = FUSE_ARGS_INIT(argc, argv); 
   struct fuse_operations hello_oper = {
   };
+  std::string target_str = "localhost:50051";
+  GreeterClient greeter(
+      grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+  std::string user("world");
+  std::string reply = greeter.SayHello(user);
+  std::cout << "Greeter received: " << reply << std::endl;  
   // ret = fuse_main(argc, argv, &hello_oper, NULL);
 }
